a30672490948de46ca92877953f74c018310e6db
bug1vuge <malutinm11@gmail.com>
2021-11-27 18:14:33 +0600

    внес правки в код модального окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,11 +1,10 @@
 const modalModule = () => {
     const overlay = document.querySelector('.overlay');
 
-    const toggleModal = (modalSelector, openButtonSelector, closeButtonSelector) => {
+    const toggleModal = (modalSelector, openButtonSelector) => {
 
         const modal = document.querySelector(modalSelector);
         const openModalButtons = document.querySelectorAll(openButtonSelector);
-        const closeModalButtons = document.querySelectorAll(closeButtonSelector);
 
 
         openModalButtons.forEach(el => {
@@ -19,21 +18,26 @@ const modalModule = () => {
             });
         });
 
-        closeModalButtons.forEach(el => {
-            el.addEventListener('click', (e) => {
-                if (e.target.tagName === 'A' || e.target.tagName === 'BUTTON') {
-                    e.preventDefault();
-                };
 
+        modal.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            if (e.target.title === 'Close') {
                 overlay.style.display = 'none';
                 modal.style.display = 'none';
-            });
+            }
+
+        });
+
+        overlay.addEventListener('click', () => {
+            overlay.style.display = 'none';
+            modal.style.display = 'none';
         });
     };
 
 
-    toggleModal('.header-modal', '.order-call-btn', '.header-modal__close');
-    toggleModal('.services-modal', '.call-measurer-btn', '.services-modal__close');
+    toggleModal('.header-modal', '.order-call-btn');
+    toggleModal('.services-modal', '.call-measurer-btn');
 };
 
 export default modalModule;
